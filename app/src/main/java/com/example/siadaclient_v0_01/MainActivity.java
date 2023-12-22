22b03571b85d83d8376ae636ad821a68e40a76fc
Yevhenii Marchuk <gentelmensatlas2020@gmail.com>
2023-12-22 10:18:03 +0200

    рефактор кода

--- app/src/main/java/com/example/siadaclient_v0_01/MainActivity.java
+++ app/src/main/java/com/example/siadaclient_v0_01/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.siadaclient_v0_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
@@ -26,9 +30,14 @@ public class MainActivity extends AppCompatActivity {
     MqttAndroidClient client;
     MqttConnectOptions options;
 
+
+
+
     static String HOST = "tcp://10.147.20.12:1883";
     static String USERNAME = "mosquitto";
     static String PASSWORD = "202020";
+
+    final String TAG = "lifecycle";
 
     private int lightSwitch = 0;
 
@@ -36,17 +45,25 @@ public class MainActivity extends AppCompatActivity {
     private ImageView light;
     private TextView temp;
     private TextView hum;
-    private ImageView settings;
+    private SharedPreferences bufferPrefs;
+
+    private final String TempKey = "SavedTemp";
+    private final String HumKey = "SavedHum";
+
+
+    //Старт актівіті
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Activity створено");
 
         light = findViewById(R.id.lightOffId);
         temp = findViewById(R.id.tempTextId);
         hum = findViewById(R.id.tempHumId);
-        settings = findViewById(R.id.settingsButton);
+        ImageView settings = findViewById(R.id.settingsButton);
 
         String clientId = MqttClient.generateClientId();
         client = new MqttAndroidClient(this.getApplicationContext(), HOST, clientId);
@@ -55,34 +72,9 @@ public class MainActivity extends AppCompatActivity {
         options.setUserName(USERNAME);
         options.setPassword(PASSWORD.toCharArray());
 
-
-        try {
-            IMqttToken token = client.connect(options);
-            token.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    Toast.makeText(MainActivity.this, "connected", Toast.LENGTH_LONG).show();
-                    SUBSCRIBE(client, "ledControlFeedback");
-                    SUBSCRIBE(client, "RoomTemp");
-                    SUBSCRIBE(client, "RoomHud");
-
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Toast.makeText(MainActivity.this, "connection fell", Toast.LENGTH_LONG).show();
-
-                }
-            });
-
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-
-        settings.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-            startActivity(intent);
-        }) ;
+        INIT();
+        CONNECT();
+        CALLBACK();
 
         light.setOnClickListener(view -> {
 
@@ -110,7 +102,65 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        settings.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        }) ;
 
+
+    }
+
+    // END
+
+    public void SUBSCRIBE(MqttAndroidClient client, String topic) {
+        int qos = 1;
+        try {
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void CONNECT() {
+        try {
+            IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(MainActivity.this, "connected", Toast.LENGTH_LONG).show();
+                    SUBSCRIBE(client, "ledControlFeedback");
+                    SUBSCRIBE(client, "RoomTemp");
+                    SUBSCRIBE(client, "RoomHud");
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MainActivity.this, "connection fell", Toast.LENGTH_LONG).show();
+
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    public void CALLBACK() {
         client.setCallback(new MqttCallback() {
             @Override
             public void connectionLost(Throwable cause) {
@@ -135,14 +185,24 @@ public class MainActivity extends AppCompatActivity {
 
                 if (topic.equals("RoomTemp")) {
                     String data = message.toString();
-                    temp.setText(data + "°C");
+                    String[] parts = data.split("\\.");
+                    String part1 = parts[0];
+                    SharedPreferences.Editor edit = bufferPrefs.edit();
+                    edit.putString(TempKey, part1);
+                    edit.apply();
+
+                    temp.setText(part1 + "°C");
 
                 }
 
                 if (topic.equals("RoomHud")) {
                     String data = message.toString();
-                    hum.setText(data + "%");
-
+                    String[] parts = data.split("\\.");
+                    String part1 = parts[0];
+                    SharedPreferences.Editor edit = bufferPrefs.edit();
+                    edit.putString(HumKey, part1);
+                    edit.apply();
+                    hum.setText(part1 + "%");
                 }
 
             }
@@ -152,34 +212,47 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    public void INIT() {
+        bufferPrefs = getSharedPreferences("showrooms", MODE_PRIVATE);
+        temp.setText(new StringBuilder().append(bufferPrefs.getString(TempKey, "00")).append("°C").toString());
+        hum.setText(new StringBuilder().append(bufferPrefs.getString(HumKey, "00")).append("%").toString());
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Activity з'являється");
 
 
 
     }
 
-    public void SUBSCRIBE (MqttAndroidClient client, String topic) {
-        int qos = 1;
-        try {
-            IMqttToken subToken = client.subscribe(topic, qos);
-            subToken.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Activity в фокусі");
 
-                }
 
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+    }
 
-                }
-            });
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Activity призупинено");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Activity зупинено");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "Activity знищено");
     }
 }
